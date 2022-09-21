c5cdcd7992717cce69faa900ef6898cfc332b182
franlops <franlopbri@gmail.com>
2022-09-21 12:07:11 -0500

    App Magic Ball

---
+++ app.js
@@ -0,0 +1,38 @@
+const ask = () => {
+    alert('Holaaaa!');
+    //Leer Pregunta
+    //Recibir respuesta al preguntar a bola mágina
+    //Imprimir Resultado
+};
+
+//Función Bola Mágica
+const magicBall = () => {
+    let randomNumber = Math.floor(Math.random() * 8);
+    let eightBall = '';
+    switch (randomNumber){
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eighBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+    }
+};
